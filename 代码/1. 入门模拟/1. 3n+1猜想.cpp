ae1c67ab0bac88149a5b6ede28eef60d5cff7c91
prhz <1457049193@qq.com>
2021-01-09 23:24:45 +0800

    add cpp

---
+++ 代码/1. 入门模拟/1. 3n+1猜想.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+	int n;
+	int step = 0;
+	scanf("%d", &n);
+	while(n != 1)
+	{
+		if(n%2==0) // 偶数
+		{
+			n = n / 2;
+		}else{
+			n = (3*n+1) / 2;
+		}
+		step++;
+	}
+	printf("%d\n", step);
+}
